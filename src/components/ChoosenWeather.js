fe12d2d88aedbc65d9a10dfb59e6e1ce39413865
novkri <nov.kri@yandex.ru>
2021-03-25 03:44:24 +0700

    style

--- src/components/ChoosenWeather.js
+++ src/components/ChoosenWeather.js
@@ -6,56 +6,44 @@ import RoundTemp from './RoundTemp'
 const ChoosenWeather = ({ day }) => {
   console.log(day);
   return ( 
-    <div className="picked-weather-container">
+    <div className="picked-container">
+      <div className="picked-header">
+        <h3 className="weather-date">
+          <DateDisplay UTCDate={day.dt} checkIfToday={true} />
+        </h3>
+        <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png`} alt={day.weather[0].main} />
+        <p>{day.weather[0].description}</p>
+        <p>max: <RoundTemp temp={day.temp.max} />, min: <RoundTemp temp={day.temp.min} /> </p>
+      </div>
 
-
-
-      <h3 className="weather-date">
-        <DateDisplay UTCDate={day.dt} />
-      </h3>
-
-
-
-      <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png`} alt={day.weather[0].main} />
-      <p>{day.weather[0].description}</p>
-
-
-
-      <p>max: <RoundTemp temp={day.temp.max} />, min: <RoundTemp temp={day.temp.min} /> </p>
-
-      <div className="weather-table">
-        <div className="actual-weather weather-col">
-          <p><strong>Температура</strong></p>
-          <p><strong>Утром: </strong> <RoundTemp temp={day.temp.morn} /></p>
-          <p><strong>Днем: </strong> <RoundTemp temp={day.temp.day} /></p>
-          <p><strong>Вечером: </strong> <RoundTemp temp={day.temp.eve} /></p>
-          <p><strong>Ночью: </strong> <RoundTemp temp={day.temp.night} /></p>
+      <div className="picked-info">
+        <div className="weather-table">
+          <div className="actual-weather weather-col">
+            <p><strong>Температура</strong></p>
+            <p><strong>Утром: </strong> <RoundTemp temp={day.temp.morn} /></p>
+            <p><strong>Днем: </strong> <RoundTemp temp={day.temp.day} /></p>
+            <p><strong>Вечером: </strong> <RoundTemp temp={day.temp.eve} /></p>
+            <p><strong>Ночью: </strong> <RoundTemp temp={day.temp.night} /></p>
+          </div>
+          <div className="feels-like weather-col">
+            <p><strong>Ощущается как</strong></p>
+            <p><strong>Утром: </strong> <RoundTemp temp={day.feels_like.morn} /></p>
+            <p><strong>Днем: </strong> <RoundTemp temp={day.feels_like.day} /></p>
+            <p><strong>Вечером: </strong> <RoundTemp temp={day.feels_like.eve} /></p>
+            <p><strong>Ночью: </strong> <RoundTemp temp={day.feels_like.night} /></p>
+          </div>
         </div>
-        <div className="feels-like weather-col">
-          <p><strong>Ощущается как</strong></p>
-          <p><strong>Утром: </strong> <RoundTemp temp={day.feels_like.morn} /></p>
-          <p><strong>Днем: </strong> <RoundTemp temp={day.feels_like.day} /></p>
-          <p><strong>Вечером: </strong> <RoundTemp temp={day.feels_like.eve} /></p>
-          <p><strong>Ночью: </strong> <RoundTemp temp={day.feels_like.night} /></p>
+
+        <div className="details">
+          <p>Влажность: {day.humidity} % ?</p>
+          <p>Вероятность выпадения осадков: {day.pop}</p>
+          <p>Восход:  <DateDisplay isDay={false} UTCDate={day.sunrise} options={{hour: 'numeric', minute: '2-digit'}} /></p>
+          <p>Закат:  <DateDisplay isDay={false} UTCDate={day.sunset} options={{hour: 'numeric', minute: '2-digit'}} /></p>
+          <p>УФ-индекс: {day.uvi} (округлить, низкий?)</p>
+          <p>Ветер:  {day.wind_speed}(в м/с)  {day.wind_deg}(направление)</p>
         </div>
       </div>
 
-      <div className="details">
-        <p>Влажность: {day.humidity} % ?</p>
-        <p>Вероятность выпадения осадков: {day.pop}</p>
-        <p>Восход:  <DateDisplay isDay={false} UTCDate={day.sunrise} options={{hour: 'numeric', minute: '2-digit'}} /></p>
-        <p>Закат:  <DateDisplay isDay={false} UTCDate={day.sunset} options={{hour: 'numeric', minute: '2-digit'}} /></p>
-        <p>УФ-индекс: {day.uvi} (округлить, низкий?)</p>
-        <p>Ветер:  {day.wind_speed}(в м/с)  {day.wind_deg}(направление)</p>
-      </div>
-      {/*
-
-      
-
-      weather: 
-description: {day.weather[0].description}
-icon: "13d"
-      </div> */}
     </div>
    );
 }

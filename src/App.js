a85f21c6c64acc74bc52f289ff8788b8f547eaa0
novkri <nov.kri@yandex.ru>
2021-03-26 15:30:13 +0700

    fetch function

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ function App() {
   const [timezone, setTimezone] = useState('')
   const [oneDayForecast, setOneDayForecast] = useState([])
   const [clickedCard, setClickedCard] = useState()
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false);
+
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(success);
+  //   }
+  //   function success(position) {
+  //     localStorage.setItem("USER_LATITUDE", position.coords.latitude);
+  //     localStorage.setItem("USER_LONGITUDE", position.coords.longitude);
+  //   }
+  // }, [])
+
+  const lat = localStorage.getItem("USER_LATITUDE")
+  const lon = localStorage.getItem("USER_LONGITUDE")
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -20,19 +34,26 @@ function App() {
       localStorage.setItem("USER_LATITUDE", position.coords.latitude);
       localStorage.setItem("USER_LONGITUDE", position.coords.longitude);
     }
-  }, [])
 
-  const lat = localStorage.getItem("USER_LATITUDE")
-  const lon = localStorage.getItem("USER_LONGITUDE")
+    const fetchForecast = async () => {
+      setIsError(false)
+      setIsLoading(true)
 
-  useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&&exclude=minutely&lang=ru&appid=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(data => {
-        setIsLoading(false)
+      try {
+        await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&&exclude=minutely&lang=ru&appid=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(data => {
+        // setIsLoading(false)
         setTimezone(data.timezone)
         setForecast(data.daily)
-      })
+        })
+      } catch (error) {
+        setIsError(true)
+      }
+      setIsLoading(false)
+    }
+
+    fetchForecast()
   }, [lat, lon])
 
   const showDetails = (e) => {
@@ -41,22 +62,19 @@ function App() {
     setClickedCard(e)
   }
 
-  const renderBody = () => {
-    if (isLoading) {
-      return <Loader />
-    }
-    return (
-      <>
-        <h2 className="timezone">{timezone}</h2>
-        <WeatherCard forecast={forecast} onClickCard={showDetails} currentClickedCard={clickedCard} />
-        { clickedCard &&  <ChoosenWeather day={oneDayForecast} />}
-      </>
-    )
-  }
-
   return (
     <div className="App">
-     {renderBody()}
+      {isError && <div>ЧТо-то пошло не так...</div>}
+
+      { isLoading ? 
+        <Loader /> : (
+          <>
+            <h2 className="timezone">{timezone}</h2>
+            <WeatherCard forecast={forecast} onClickCard={showDetails} currentClickedCard={clickedCard} />
+            { clickedCard &&  <ChoosenWeather day={oneDayForecast} />}
+          </>
+        )
+      }
     </div>
   );
 }
